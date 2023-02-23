b80642247ccbbc82f7d31d5dda8d834d1b8c5295
Daniel Rivas <31682816+danieldotwav@users.noreply.github.com>
2023-02-23 21:47:04 +0000

    works now

--- .ccls-cache/@home@runner@Week-3-RivasDaniel/main.cpp
+++ .ccls-cache/@home@runner@Week-3-RivasDaniel/main.cpp
@@ -3,19 +3,43 @@ Lab: Week 3
 Student first name: Daniel
 Student last name: Rivas
 ***********************/
+#include <iostream>
+#include "includes/student.h"
+using namespace std;
 
+const int AP_STUDENTS = 2;
+const int NBR_STUDENTS = 3;
 
 int main() {
+  // Variable Declarations
+  Student students_ap[AP_STUDENTS] = {Student("Mike", "Miller"),
+                                      Student("Linda", "Lawson")};
+  Student students[NBR_STUDENTS];
 
   cout << "Enter scores for AP students\n";
+  for(int i = 0; i < AP_STUDENTS; i++){
+    students_ap[i].setScores();
+  }
 
   cout << "Show scores for AP students\n";
+  for(int i = 0; i < AP_STUDENTS; i++){
+    students_ap[i].showScores();
+  }
 
   cout << "Enter names for non-AP students\n";
+  for(int i = 0; i < NBR_STUDENTS; i++){
+    students[i].updateName();
+  }
 
   cout << "Enter scores for non-AP students\n";
+  for(int i = 0; i < NBR_STUDENTS; i++){
+    students[i].setScores();
+  }
 
   cout << "Show scores for non-AP students\n";
+  for(int i = 0; i < NBR_STUDENTS; i++){
+    students[i].showScores();
+  }
 
   cout << "\nGoodbye\n";
   return 0;
