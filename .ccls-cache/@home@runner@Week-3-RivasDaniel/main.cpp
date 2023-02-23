42018180f15456069f23358fadd4125967f88e8f
Daniel Rivas <31682816+danieldotwav@users.noreply.github.com>
2023-02-23 04:19:33 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Week-3-RivasDaniel/main.cpp
@@ -0,0 +1,96 @@
+/**********************
+Lab: Week 3
+Student first name: Daniel
+Student last name: Rivas
+***********************/
+
+
+int main() {
+
+  cout << "Enter scores for AP students\n";
+
+  cout << "Show scores for AP students\n";
+
+  cout << "Enter names for non-AP students\n";
+
+  cout << "Enter scores for non-AP students\n";
+
+  cout << "Show scores for non-AP students\n";
+
+  cout << "\nGoodbye\n";
+  return 0;
+}
+
+/**********************************
+// YOUR OUTPUT
+
+// SAMPLE OUTPUT
+Enter scores for AP students
+Enter score 1 for student 101: Mike Miller
+100
+Enter score 2 for student 101: Mike Miller
+100
+Enter score 3 for student 101: Mike Miller
+99
+Enter score 4 for student 101: Mike Miller
+98
+Enter score 1 for student 102: Linda Lawson
+100
+Enter score 2 for student 102: Linda Lawson
+98
+Enter score 3 for student 102: Linda Lawson
+99
+Enter score 4 for student 102: Linda Lawson
+92
+Show scores for AP students
+Scores for student 101: Mike Miller are
+100 100 99 98
+The average is: 99
+Scores for student 102: Linda Lawson are
+100 98 99 92
+The average is: 97
+Enter names for non-AP students
+Enter first name for student 103: Mary
+Enter last name for student 103: Mills
+Enter first name for student 104: Jacob
+Enter last name for student 104: Johnson
+Enter first name for student 105: Tim
+Enter last name for student 105: Thomas
+Enter scores for non-AP students
+Enter score 1 for student 103: Mary Mills
+98
+Enter score 2 for student 103: Mary Mills
+97
+Enter score 3 for student 103: Mary Mills
+96
+Enter score 4 for student 103: Mary Mills
+95
+Enter score 1 for student 104: Jacob Johnson
+87
+Enter score 2 for student 104: Jacob Johnson
+86
+Enter score 3 for student 104: Jacob Johnson
+85
+Enter score 4 for student 104: Jacob Johnson
+84
+Enter score 1 for student 105: Tim Thomas
+76
+Enter score 2 for student 105: Tim Thomas
+75
+Enter score 3 for student 105: Tim Thomas
+74
+Enter score 4 for student 105: Tim Thomas
+73
+Show scores for non-AP students
+Scores for student 103: Mary Mills are
+98 97 96 95
+The average is: 96
+Scores for student 104: Jacob Johnson are
+87 86 85 84
+The average is: 85
+Scores for student 105: Tim Thomas are
+76 75 74 73
+The average is: 74
+
+Goodbye
+***********************************/
